c072968d10d9ef0fa9b2f27a59a81ed1ac5ff5c3
timborovkov <tim.borovkov@icloud.com>
2021-11-20 12:46:07 +0200

    Init

--- OneTwoOneDeals/.prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  bracketSpacing: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  printWidth: 100,
-  jsxBracketSameLine: false,
-};
--- OneTwoOneDeals/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Root from './src/index';
-import configureStore from './src/store/index';
-
-const { persistor, store } = configureStore();
-
-export default function App() {
-  return <Root store={store} persistor={persistor} />;
-}
